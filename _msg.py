d2e00b5835dd22993abc5316cb53b7d8853d9ebd
Epicarno <danir_02@mail.ru>
2026-02-10 16:50:53 +0500

    chore: remove temp files

--- _msg.py
+++
@@ -1,5 +0,0 @@
-from pathlib import Path
-Path(__file__).with_name(".commitmsg").write_text(
-    "feat: \u0444\u0438\u043b\u044c\u0442\u0440 \u0448\u043a\u0430\u0444\u043e\u0432 cabinets.txt + \u0441\u0431\u043e\u0440\u043a\u0430 \u0434\u0435\u043f\u043b\u043e\u044f collect_output.py",
-    encoding="utf-8"
-)
